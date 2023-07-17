5cf9625c0c13b7aaca96fe3daa1e6e6c5394354b
Tomas Votruba <tomas.vot@gmail.com>
2023-07-17 13:53:31 +0200

    cs

--- config/app.php
+++ config/app.php
@@ -1,9 +1,10 @@
 <?php
 
 declare(strict_types=1);
+use TomasVotruba\ClassLeak\Providers\AppServiceProvider;
 
 use TomasVotruba\PunchCard\AppConfig;
 
 return AppConfig::make()
-    ->providers([\TomasVotruba\ClassLeak\Providers\AppServiceProvider::class])
+    ->providers([AppServiceProvider::class])
     ->toArray();
