0c415b31439a98735ce8317c2d593efc48436af5
Tomas Votruba <tomas.vot@gmail.com>
2023-07-18 17:34:58 +0200

    cleanup

--- scoper.php
+++ scoper.php
@@ -12,18 +12,13 @@ use Nette\Utils\Strings;
 
 // see https://github.com/humbug/php-scoper
 return [
-    'prefix' => 'EasyCI' . $timestamp,
-    'expose-classes' => [
-        // part of public interface of configs.php
-        'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
-    ],
+    'prefix' => 'ClassLeak' . $timestamp,
     'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
-    'exclude-namespaces' => ['#^Symplify\\\\EasyCI#', '#^Symfony\\\\Polyfill#'],
+    'exclude-namespaces' => ['#^TomasVotruba\\\\ClassLeak#', '#^Symfony\\\\Polyfill#'],
     'exclude-files' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
         'vendor/symfony/deprecation-contracts/function.php',
-        'stubs/PHPUnit/PHPUnit_Framework_TestCase.php',
     ],
     'patchers' => [
         // scope symfony configs
@@ -32,64 +27,11 @@ return [
                 return $content;
             }
 
-            // fix symfony config load scoping, except EasyCI
-            $content = Strings::replace(
-                $content,
-                '#load\(\'Symplify\\\\\\\\(?<package_name>[A-Za-z]+)#',
-                function (array $match) use ($prefix) {
-                    if (in_array($match['package_name'], ['EasyCI'], true)) {
-                        // skip
-                        return $match[0];
-                    }
-
-                    return 'load(\'' . $prefix . '\Symplify\\' . $match['package_name'];
-                }
-            );
-
             return $content;
         },
 
-        // unprefix test case class names
         function (string $filePath, string $prefix, string $content): string {
-            if (! str_ends_with($filePath, 'packages/Testing/UnitTestFilter.php')) {
-                return $content;
-            }
-
-            $content = Strings::replace(
-                $content,
-                '#' . $prefix . '\\\\PHPUnit\\\\Framework\\\\TestCase#',
-                'PHPUnit\Framework\TestCase'
-            );
-
-            return Strings::replace(
-                $content,
-                '#' . $prefix . '\\\\PHPUnit_Framework_TestCase#',
-                'PHPUnit_Framework_TestCase'
-            );
-        },
-
-        // unprefix kernerl test case class names
-        function (string $filePath, string $prefix, string $content): string {
-            if (! str_ends_with($filePath, 'packages/Testing/UnitTestFilter.php')) {
-                return $content;
-            }
-
-            $content = Strings::replace(
-                $content,
-                '#' . $prefix . '\\\\Symfony\\\\Bundle\\\\FrameworkBundle\\\\Test\\\\KernelTestCase#',
-                'Symfony\Bundle\FrameworkBundle\Test\KernelTestCase'
-            );
-
-            return Strings::replace(
-                $content,
-                '#' . $prefix . '\\\\Symfony\\\\Component\\\\Form\\\\Test\\\\TypeTestCase',
-                'Symfony\Component\Form\Test\TypeTestCase'
-            );
-        },
-
-        // unprefix string class names to ignore, to keep original class names
-        function (string $filePath, string $prefix, string $content): string {
-            if (! str_ends_with($filePath, 'packages/ActiveClass/Filtering/PossiblyUnusedClassesFilter.php')) {
+            if (! str_ends_with($filePath, 'app/Filtering/PossiblyUnusedClassesFilter.php')) {
                 return $content;
             }
 
