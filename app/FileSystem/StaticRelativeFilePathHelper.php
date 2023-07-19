b47f1e72afdb2b52ca110bc2e3c8e63032cc5e04
Tomas Votruba <tomas.vot@gmail.com>
2023-07-19 05:13:41 +0200

    make file path symfony independent

--- app/FileSystem/StaticRelativeFilePathHelper.php
+++ app/FileSystem/StaticRelativeFilePathHelper.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace TomasVotruba\ClassLeak\FileSystem;
 
-use Symfony\Component\Filesystem\Filesystem;
-
 final class StaticRelativeFilePathHelper
 {
     public static function resolveFromCwd(string $filePath): string
     {
-        $filesystem = new Filesystem();
-        $relativeFilePathFromCwd = $filesystem->makePathRelative((string) realpath($filePath), getcwd());
-
-        return rtrim($relativeFilePathFromCwd, '/');
+        // make path relative with native PHP
+        $relativeFilePath = (string) realpath($filePath);
+        return str_replace(getcwd() . DIRECTORY_SEPARATOR, '', $relativeFilePath);
     }
 }
