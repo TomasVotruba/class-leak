57ff79a60ee00717854c7b80a56293ed467c2207
Tomas Votruba <tomas.vot@gmail.com>
2023-07-19 06:02:13 +0200

    make use project auotload ialway loaded

--- bin/class-leak.php
+++ bin/class-leak.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 use TomasVotruba\ClassLeak\Console\ClassLeakApplication;
 use TomasVotruba\ClassLeak\DependencyInjection\ContainerFactory;
 
-if (file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
-    // A. build downgraded package
-    require_once  __DIR__ . '/../vendor/scoper-autoload.php';
-} elseif (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
-    // B. dev package
-    require_once  __DIR__ . '/../../../../vendor/autoload.php';
-} else {
-    // C. local repository
-    require_once __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    // project's autoload
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
 }
 
+if (file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
+    // A. build downgraded package
+    require_once __DIR__ . '/../vendor/scoper-autoload.php';
+} else {
+    // B. local repository
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 
 $containerFactory = new ContainerFactory();
 $container = $containerFactory->create();
