d965900b561aadf5bd1ff35e0b8d067c99739e36
GitHub Action <action@github.com>
2023-07-20 11:04:42 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Session/Middleware/AuthenticatesSessions.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ClassLeak202307\Illuminate\Contracts\Session\Middleware;
+
+interface AuthenticatesSessions
+{
+    //
+}
---
+++ vendor/nikic/php-parser/lib/PhpParser/Node/Expr/AssignOp/BitwiseXor.php
@@ -0,0 +1,13 @@
+<?php
+
+declare (strict_types=1);
+namespace ClassLeak202307\PhpParser\Node\Expr\AssignOp;
+
+use ClassLeak202307\PhpParser\Node\Expr\AssignOp;
+class BitwiseXor extends AssignOp
+{
+    public function getType() : string
+    {
+        return 'Expr_AssignOp_BitwiseXor';
+    }
+}
