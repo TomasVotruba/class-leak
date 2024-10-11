6374502a8c5d60fb5496e4f281048aa06f99b794
Markus Staab <maggus.staab@googlemail.com>
2024-10-11 14:33:06 +0200

    Discard changes to build/rector-downgrade-php-72.php

--- build/rector-downgrade-php-72.php
+++ build/rector-downgrade-php-72.php
@@ -8,7 +8,7 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel(240, 8, 1);
 
-    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_74]);
+    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
     $rectorConfig->skip([
                             '*/Tests/*',
