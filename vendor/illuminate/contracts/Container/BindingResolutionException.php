0fa4d5ccfcc1896f887a484a01976596d5d500f1
GitHub Action <action@github.com>
2024-12-03 17:03:53 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Container/BindingResolutionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ClassLeak202412\Illuminate\Contracts\Container;
+
+use Exception;
+use ClassLeak202412\Psr\Container\ContainerExceptionInterface;
+class BindingResolutionException extends Exception implements ContainerExceptionInterface
+{
+    //
+}
---
+++ vendor/nikic/php-parser/lib/PhpParser/Comment/Doc.php
@@ -0,0 +1,8 @@
+<?php
+
+declare (strict_types=1);
+namespace ClassLeak202412\PhpParser\Comment;
+
+class Doc extends \ClassLeak202412\PhpParser\Comment
+{
+}
